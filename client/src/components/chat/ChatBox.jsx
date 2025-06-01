ff1f8db99bc560641ce3cfe59121351ffc944501
AkshithaRao123 <akshashw@gmail.com>
2025-06-01 20:19:06 +0530

    display messages

--- client/src/components/chat/ChatBox.jsx
+++ client/src/components/chat/ChatBox.jsx
@@ -1,6 +1,8 @@
 import { useContext } from "react"
 import { AuthContext } from "../../context/AuthContext";
 import { ChatContext } from "../../context/ChatContext";
+import {Stack} from "react-bootstrap";
+import moment from "moment";
 import { useFetchRecipientUser } from "../../hooks/useFetchRecipient";
 
 const ChatBox = () => {
@@ -15,12 +17,31 @@ const ChatBox = () => {
         )
     }
 
+    if (isMessagesLoading) {
+        return (
+            <p style={{ textAlign: "center", width: "100%" }}>Loading chats...</p>
+        )
+    }
+
     console.log("Got recipient user:", recipientUser);
 
     return (
-        <div>
-            Chatbox
-        </div>
+        <Stack gap={4} className="chat-box">
+            <div className="chat-header">
+                <strong>
+                    {recipientUser?.name}
+                </strong>
+            </div>
+            <Stack gap={3} className="messages">
+                {messages && messages.map((message, index) => 
+                <Stack key={{index}} className={`${message?.senderId === user?._id ? 
+                    "message self align-self-end flex-grow-0" : "message align-self-start flex-grow-0"
+                }`}>
+                    <span>{message.text}</span>
+                    <span className="message-footer">{moment(message.createdAt).calendar()}</span>
+                </Stack>)}
+            </Stack>
+        </Stack>
     )
 }
 
